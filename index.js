49988afd9041d837e99c5808fd2b265961b59626
Eunice Souza <ejs.12@hotmail.com>
2022-11-20 22:26:29 -0300

    Atualizando o projeto

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-let header = document.getElementById("header")
-let navigationHeader = document.getElementById("nagivation_header")
-let content = document.getElementById("content")
-let showSidebar = false
+let header = document.getElementById("header");
+let navigationHeader = document.getElementById("nagivation_header");
+let content = document.getElementById("content");
+let showSidebar = false;
 
-function toggleSidebar(){
-    showSidebar = !showSidebar
-    if (showSidebar){
-        navigationHeader.style.marginLeft = "-10vw" 
-        navigationHeader.style.animationName = "showSidebar"
-        content.style.filter = "blur(2px)"
-    }else{
-        navigationHeader.style.marginLeft = "-100vw" 
-        navigationHeader.style.animationName = ""
-        content.style.filter = ""
-    }
-
+function toggleSidebar() {
+  showSidebar = !showSidebar;
+  if (showSidebar) {
+    navigationHeader.style.marginLeft = "-10vw";
+    navigationHeader.style.animationName = "showSidebar";
+    content.style.filter = "blur(2px)";
+  } else {
+    navigationHeader.style.marginLeft = "-100vw";
+    navigationHeader.style.animationName = "";
+    content.style.filter = "";
+  }
 }
 
-function closeSidebar(){
-    if(showSidebar){
-        toggleSidebar();
-    }
+function closeSidebar() {
+  if (showSidebar) {
+    toggleSidebar();
+  }
 }
 
-window.addEventListener("resize", function(event) {
-    if(this.window.innerWidth > 768 && showSidebar){
-        toggleSidebar()
-    }
-})
+window.addEventListener("resize", function (event) {
+  if (this.window.innerWidth > 768 && showSidebar) {
+    toggleSidebar();
+  }
+});
